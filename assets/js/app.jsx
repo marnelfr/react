c5e770d22ef1a405301e95e253619769661bc1f1
Marnel Fresh'eur <gmlginolias@gmail.com>
2021-02-12 03:30:24 +0100

    building a forms in react

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,122 +1,64 @@
-function WelcomeFunc(props) {
-  return <h1>Bonjour {props.name}</h1>
+function Input({name, value, type, onChange, children}) {
+    return <div className="form-group">
+      <label htmlFor={name}>{children}</label>
+      <input type={type} value={value} name={name} id={name} onChange={onChange} className="form-control" />
+    </div>
 }
 
-class Welcome extends React.Component {
-
-
-  render() {
-    return <React.Fragment>
-      <h1>
-        {this.props.name}
-      </h1>
-      <span>{this.props.children}</span>
-    </React.Fragment>
-  }
-
+function Check({name, value, type, onChange, children}) {
+  return <div className="form-check">
+    <input type={type} checked={value} name={name} id={name} onChange={onChange} className="form-check-input" />
+    <label htmlFor={name} className="form-check-label">{children}</label>
+  </div>
 }
 
-class Clock extends React.Component {
+class Forms extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {date: new Date()}
-    this.timer = null
+    this.init = {
+      nom: '',
+      prenom: '',
+      sexe: '',
+      tel: '+229 ',
+      newsLetter: false
+    }
+    this.state = this.init
+    this.handleBlur = this.handleBlur.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  componentDidMount() {
-    this.timer = window.setInterval(this.tick.bind(this), 1000)
+  handleChange(e) {
+    const name = e.target.name
+    const type = e.target.type
+    const value = type === 'checkbox' ? e.target.checked : e.target.value
+    this.setState((state, props) => ({[name]: value}))
   }
 
-  componentwillUnmount() {
-    window.clearInterval(this.timer)
+  handleBlur(e) {
+
   }
 
-  tick() {
-    this.setState((state, props) => ({date: new Date()}))
+  handleSubmit(e) {
+    e.preventDefault()
+    this.setState((state, props) => this.init)
   }
 
   render() {
-    return <p>
-      Nous sommes le {this.state.date.toLocaleDateString()} et il est {this.state.date.toLocaleTimeString()}
-    </p>
-  }
-}
-
-class Timer extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {second: 0, paused: false, timer: null}
-    this.pause = this.pause.bind(this)
-    this.clear = this.clear.bind(this)
-  }
-
-  componentDidMount() {
-    this.startTimer()
-  }
-
-  componentwillUnmount() {
-    window.clearInterval(this.timer)
-  }
-
-  seconder() {
-    this.setState((state, props) => ({second: ++state.second}))
-  }
-
-  startTimer() {
-    this.setState((state, props) => ({timer: window.setInterval(this.seconder.bind(this), 1000)}))
-  }
-
-  stopTimer() {
-    this.setState((state, props) => {
-      window.clearInterval(state.timer)
-      return {timer: null}
-    })
-  }
-
-  clear(e) {
-    e.preventDefault()
-    this.stopTimer()
-    this.state.second = 0
-    this.startTimer()
-  }
-
-  pause(e) {
-    e.preventDefault()
-    this.setState((state, props) => {
-      if (!state.paused) {
-        this.stopTimer()
-      } else {
-        this.startTimer()
-      }
-      return {paused: !state.paused}
-    })
-  }
-
-  render() {
-    return <div>
-      <p>
-        Vous êtes sur la page depuis {this.state.second} secondes
-      </p>
-      <button onClick={this.pause}>{this.state.paused ? 'Play' : 'Pause'}</button>
-      <button onClick={this.clear}>Clear</button>
+    return <div className="container">
+      <form onSubmit={this.handleSubmit}>
+        <Input name="nom" value={this.state.nom} type="text" onChange={this.handleChange}>Nom</Input>
+        <Input name="prenom" value={this.state.prenom} type="text" onChange={this.handleChange}>Prénom</Input>
+        <Input name="tel" value={this.state.tel} type="tel" onChange={this.handleChange}>Téléphone</Input>
+        <Check name="newsLetter" value={this.state.newsLetter} type="checkbox" onChange={this.handleChange}>S'abonner au newsLetter</Check>
+        <button className="btn btn-primary">Envoyer</button>
+      </form>
+      {JSON.stringify(this.state)}
     </div>
   }
 
 }
 
 
-class Home extends React.Component {
-
-  render() {
-    return <React.Fragment>
-      <Welcome name={this.props.name}/>
-      <Clock />
-      <Timer />
-    </React.Fragment>
-  }
-
-}
-
-ReactDOM.render(<Home name={"Jean"}>Soyez les bienvenu</Home>, document.getElementById('app'))
+ReactDOM.render(<Forms />, document.getElementById('app'))
